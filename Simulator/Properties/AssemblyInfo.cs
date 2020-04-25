19c678d52ce47fc9dec587a0cc9be9ebf4941aba
Jeremy Reimert <jeremyreimert@gmail.com>
2020-04-25 13:28:13 -0400

    Add files via upload

---
+++ Simulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,48 @@
+﻿/**********************************************************************************
+ * NAME: Jeremy Reimert
+ * DATE: February 7, 2020
+ * PROJECT: SmartBuoy Simulator
+ * DESCRIPTION: This project was meant to be a user interface for viewing data from
+ * a device created to measure the quality of water. However, because this is an
+ * online course their is no access to the physical device. This application takes 
+ * the place of the device by producing random numbers, within a set range, for
+ * each of the measurements. Then, just as the actual device would, the application
+ * broadcasts the data both as a continuous stream and periodically to a database. 
+ **********************************************************************************/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SmartBuoySimulator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("SmartBuoySimulator")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3262cfa0-b118-4763-b542-c03e3b90e17c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
